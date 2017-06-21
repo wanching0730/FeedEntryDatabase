ead87ff80b7dbfab872041c245eaa897679de5bd
Wan Ching <wanching_97@hotmail.com>
2017-06-22 00:25:42 +0800

    read data from edittext and store to DB

--- app/src/main/java/com/example/android/database/MainActivity.java
+++ app/src/main/java/com/example/android/database/MainActivity.java
@@ -1,40 +1,60 @@
 package com.example.android.database;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
 
+import com.example.android.database.data.FeedReaderContract;
 import com.example.android.database.data.FeedReaderDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button viewAmount;
+    private EditText entryId_field, title_field, subtitle_field;
+    private int entryId;
+    private String title, subtitle;
+    private FeedReaderDbHelper helper;
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        entryId_field = (EditText) findViewById(R.id.entry_id);
+        title_field = (EditText) findViewById(R.id.title);
+        subtitle_field = (EditText) findViewById(R.id.subtitle);
+
+        Log.v("view", "view");
+
         viewAmount = (Button) findViewById(R.id.view_amount);
         viewAmount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                addData(v);
+
                 Intent intent = new Intent(MainActivity.this, DisplayDbAmount.class);
                 startActivity(intent);
-
-                SQLiteDatabase mydatabase = openOrCreateDatabase("FeedReader.db",MODE_PRIVATE,null);
-               // mydatabase.execSQL("INSERT INTO Entry VALUES(1, 'wc', 'wc')");
-                mydatabase.execSQL("INSERT INTO Entry VALUES(2, 'teoh', 'teoh')");
-
-
-
-
             }
         });
 
 
+    }
+
+    public void addData(View view){
+        entryId = Integer.parseInt(entryId_field.getText().toString().trim());
+        title = title_field.getText().toString().trim();
+        subtitle = subtitle_field.getText().toString().trim();
+        helper = new FeedReaderDbHelper(this);
+        database = helper.getWritableDatabase();
+        helper.insertData(entryId, title, subtitle, database);
+        database.close();
     }
 }
--- app/src/main/java/com/example/android/database/data/FeedReaderDbHelper.java
+++ app/src/main/java/com/example/android/database/data/FeedReaderDbHelper.java
@@ -1,7 +1,9 @@
 package com.example.android.database.data;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import android.content.Context;
 
@@ -13,10 +15,10 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "FeedReader.db";
-    private static final String TEXT_TYPE = "TEXT";
+    private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + "(" + FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + "(" + FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
                     FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP + FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + ")";
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
@@ -28,6 +30,8 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
+        Log.e("DATABASE OPERATION", "DATABASE CREATED...");
+        db.close();
     }
 
     @Override
@@ -40,5 +44,16 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public void insertData(int entryId, String title, String subtitle, SQLiteDatabase db){
+
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ENTRY_ID, entryId);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, title);
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, subtitle);
+
+        db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
+        db.close();
     }
 }
